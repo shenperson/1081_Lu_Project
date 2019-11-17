0f8e6d228d19097403f4c3a51f52e704d5793a58
shenperson <wayne19990609@gmail.com>
2019-11-18 04:41:35 +0800

    hw5

---
+++ 1081_HW5/maze.v
@@ -0,0 +1,174 @@
+`define SIZE 64
+`define MASK 16'b0000000000001111
+
+// action_o definition
+`define ACT_IDLE 2'b00 
+`define ACT_INPUT 2'b01 
+`define ACT_CAL 2'b10 
+`define ACT_OUTPUT 2'b11
+
+// state definition
+`define IDLE 2'd0
+`define INPUT 2'd1
+`define BFS 2'd2
+`define OUTPUT 2'd3
+
+// map definition
+`define START 2'b10
+`define WALL 2'b01
+`define ROAD 2'b00
+`define END 2'b11  // frontier
+
+// direction definition
+`define UP 2'b00
+`define DOWN 2'b01
+`define LEFT 2'b10
+`define RIGHT 2'b11
+
+module maze(clk, reset, start_i, row_i, action_o, coord_o);
+    
+
+    input clk,reset, start_i;
+    input [15:0] row_i;
+
+    output reg [1:0] action_o;
+    output reg [5:0] coord_o;
+
+    reg [1:0] map [`SIZE-1:0];
+    reg [1:0] direction [`SIZE-1:0];
+    reg [1:0] state, n_state;
+    reg [5:0] cnt, n_cnt;
+    reg [1:0] n_action;
+    reg [5:0] n_coord;
+    // reg [3:0] start_x, start_y, end_x, end_y;
+    reg [5:0] START, END;
+    reg [7:0] i;
+
+    // combinational
+    always @(*) begin
+        case (state)
+            `IDLE: begin
+                n_state = start_i ? `INPUT : `IDLE;
+                n_action = start_i ? `ACT_INPUT : `ACT_IDLE;
+                n_cnt = 1'b0;
+                n_coord = 6'b0;
+            end
+            `INPUT:begin
+                if(cnt!=8) begin
+                    for(i = 0; i != 8; i = i + 1) begin
+                        map[cnt+i*8] = row_i >> (14-i-i);
+                    end
+                end
+                else begin
+                    // start_x = row_i >> 12;
+                    // start_y = row_i >> 8;
+                    // end_x = row_i >> 4;
+                    // end_y = row_i;
+                    START = (row_i >> 12) * 8 + (row_i >> 8 & `MASK);
+                    END = (row_i >> 4 & `MASK) * 8 + (row_i & `MASK);
+                end
+                n_cnt = cnt + 1;
+                n_state = (cnt == 8) ? `BFS : `INPUT;
+                n_action = (cnt == 8) ? `ACT_CAL : `ACT_INPUT;
+            end
+            `BFS:begin
+                n_state = `BFS;
+                for(i = 64; i!=64;i=i+1) begin
+                    if(map[i]==`END) begin
+                        // right
+                        if(i < 56) begin
+                            if(map[i+8] == `ROAD) begin
+                                map[i+8] = `END;
+                                direction[i+8] = `LEFT;
+                            end
+                            if(map[i+8] == `START) begin
+                                direction[i+8] = `LEFT;
+                                n_state = `OUTPUT;
+                                n_action = `ACT_OUTPUT;
+                                n_coord = START;
+                            end
+                        end
+                        // left
+                        if(i > 7) begin
+                            if(map[i-8] == `ROAD) begin
+                                map[i-8] = `END;
+                                direction[i-8] = `RIGHT;
+                            end
+                            if(map[i-8] == `START) begin
+                                direction[i-8] = `RIGHT;
+                                n_state = `OUTPUT;
+                                n_action = `ACT_OUTPUT;
+                                n_coord = START;
+                            end
+                        end
+                        // UP
+                        if(i % 8 != 0) begin
+                            if(map[i-1] == `ROAD) begin
+                                map[i-1] = `END;
+                                direction[i-1] = `DOWN;
+                            end
+                            if(map[i-1] == `START) begin
+                                direction[i-1] = `DOWN;
+                                n_state = `OUTPUT;
+                                n_action = `ACT_OUTPUT;
+                                n_coord = START;
+                            end
+                        end
+                        // DOWN
+                        if(i % 8 != 7) begin
+                            if(map[i+1] == `ROAD) begin
+                                map[i+1] = `END;
+                                direction[i+1] = `UP;
+                            end
+                            if(map[i+1] == `START) begin
+                                direction[i+1] = `UP;
+                                n_state = `OUTPUT;
+                                n_action = `ACT_OUTPUT;
+                                n_coord = START;
+                            end
+                        end
+                        map[i] = `WALL;
+                    end
+                end
+            end
+            `OUTPUT: begin
+                if(coord_o != END) begin
+                    if (direction[coord_o] == `UP) begin
+                            n_coord = coord_o - 1;
+                    end
+                    else if (direction[coord_o] == `DOWN) begin
+                            n_coord = coord_o + 1;
+                    end
+                    else if (direction[coord_o] == `LEFT) begin
+                            n_coord = coord_o - 8;
+                    end
+                    else if (direction[coord_o] == `RIGHT) begin
+                            n_coord = coord_o + 8;
+                    end
+                end
+                else begin
+                    
+                end
+            end
+            default: begin
+                
+            end
+        endcase
+    end
+
+    // sequential
+    always @(posedge clk or posedge reset) begin
+        if(reset) begin
+            state <= `IDLE;
+            action_o <= `ACT_IDLE;
+        end
+        else begin
+            state <= n_state;
+            action_o <= n_action;
+            cnt <= n_cnt;
+            if(cnt==8) begin
+                $display(START," ",END);
+            end
+        end
+    end
+endmodule
---
+++ 1081_HW5/maze_tb2.v
@@ -0,0 +1,164 @@
+`timescale 1ns/10ps
+
+`define CYCLE    10
+// modify if need
+`define ENDCYCLE 1000
+
+`define MAZE          "./maze2.dat"       
+`define GOLDEN_PATH   "./golden_path2.dat" 
+
+// color definition
+`define YEL "%c[0;33m",27
+`define BLK "%c[0;m",27
+
+module test;
+
+  parameter MAZE_N_PAT = 8;
+  parameter OUT_LENGTH = 11;
+
+  reg clk;
+  reg reset;
+  reg [15:0] row_i;
+  reg start_i;
+  wire [1:0] action_o;
+  wire [5:0] coord_o;
+
+  reg [0:15] maze_mem [0:MAZE_N_PAT];
+  reg [5:0] out_mem   [0:OUT_LENGTH-1];
+  reg [5:0] out_temp;
+  integer i, j, out_f, err;
+  integer pat;
+  integer path_num;
+  integer over;
+
+  maze DUT(
+    .clk(clk),
+	.reset(reset), 
+	.start_i(start_i), 
+	.row_i(row_i), 
+	.action_o(action_o), 
+	.coord_o(coord_o)
+  );
+  
+
+// -------------------------------------------------------- //
+//   Global behavior
+// -------------------------------------------------------- //
+
+always #(`CYCLE/2) clk = ~clk;
+
+initial begin
+  $readmemb (`MAZE,  maze_mem);
+  $readmemb (`GOLDEN_PATH, out_mem);
+  $display("\n =================== 2019 Fall HW5 - Maze =================== ");
+
+  clk = 1;
+  reset = 0;
+  start_i = 0;
+  pat = 0;
+  path_num = 0;
+  over = 0;
+  err = 0;    
+   
+  //reset
+  #(`CYCLE*1.5 + 1) reset = 1;
+  #(`CYCLE/2) reset = 0;
+end
+
+initial begin
+  `ifdef SDFSYN
+    $sdf_annotate("maze_syn.sdf", DUT);
+  `endif
+  `ifdef SDFAPR
+    $sdf_annotate("maze_APR.sdf", DUT);
+  `endif
+  `ifdef FSDB
+    $fsdbDumpfile("maze.fsdb");	
+    $fsdbDumpvars();
+	//if you want to dump register array ex. reg [0:15] reg_arr [0:7] use $fsdbDumpvars(0, DUT, "+mda") instead.
+    //$fsdbDumpvars(0, DUT, "+mda");
+  `endif
+end
+
+always@(negedge clk)
+begin
+  if(action_o === 2'b00) begin
+    start_i= 1'b1;   
+  end
+  else begin
+    start_i= 1'b0;   
+  end
+  if((action_o === 2'b01) && pat<=MAZE_N_PAT) begin  
+	//start_i= 1'b1;         
+	row_i = maze_mem[pat]; 
+	pat = pat + 1;
+  end 
+  else begin
+	//start_i= 1'b0;         
+	row_i = 16'b0; 
+  end 
+end
+
+// Finish
+initial begin
+  #(`CYCLE*`ENDCYCLE);
+  $display(" =================== TIME OUT ======================\n");
+  $display("         There are someting wrong in your code...  \n");
+  $display("           If you need more simulation time         ");
+  $display("  You can try to modify ENDCYCLE in the testbench .\n");
+  $display(" =================== TIME OUT ======================\n");
+  $finish;
+end
+
+always @(negedge clk)
+begin
+  out_temp = out_mem[path_num];
+  if (action_o === 2'b11)
+    begin
+	  if (coord_o === out_temp)
+        $display("At %5d NS", $time, `YEL," Correct!",`BLK, " Step %d : Expect: (%d,%d), Yours (%d,%d)", path_num, out_temp[5:3], out_temp[2:0], coord_o[5:3], coord_o[2:0]);
+      else
+        begin
+          $display("At %5d NS", $time, " Error!   Step %d : Expect: (%d,%d), Yours (%d,%d)", path_num, out_temp[5:3], out_temp[2:0], coord_o[5:3], coord_o[2:0]);
+          err = err + 1 ;
+        end	  
+      path_num = path_num + 1;	            
+    end  
+  if (path_num === OUT_LENGTH) 
+    begin 
+	  over = 1;
+	end
+end
+
+always @(negedge clk)
+begin
+  if(over === 1) begin
+    if(err==0) begin
+        $display("\n");
+        $display("                   ****************************              ");
+        $display("                   **                        **        /|__/|");
+        $display("                   **  Congratulations !!    **      / O,O  |");
+        $display("                   **                        **    /_____   |");
+        $display("                   **  You pass this test!!  **   /^ ^ ^ \\  |");
+        $display("                   **                        **  |^ ^ ^ ^ |w|");
+        $display("                   ****************************   \\m___m__|_|");
+        $display("\n");	
+        $finish;
+    end
+    else begin
+        $display("\n");
+        $display("                   ****************************              ");
+        $display("                   **      *       *         **        /|__/|");
+        $display("                   **        *   *           **      / >,<  |");
+        $display("                   **          *             **    /_____   |");
+        $display("                   **        *   *           **   /^ ^ ^ \\  |");
+        $display("                   **      *       *         **  |^ ^ ^ ^ |w|");
+        $display("                   ****************************   \\m___m__|_|");
+        $display("\n");
+        $display("There are %d errors in your results\n", err);	
+        $finish;      
+    end
+  end
+end  
+
+endmodule
